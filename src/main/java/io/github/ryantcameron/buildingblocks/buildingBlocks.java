38aa12166a04d4d1741a84e5215cf5cba25d0467
ryca1116 <37085161+ryca1116@users.noreply.github.com>
2019-08-04 22:21:45 -0600

    Initial commit

--- src/main/java/io/github/ryantcameron/buildingblocks/buildingBlocks.java
+++ src/main/java/io/github/ryantcameron/buildingblocks/buildingBlocks.java
@@ -1,7 +1,23 @@
 package io.github.ryantcameron.buildingblocks;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class buildingBlocks extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		getLogger().info("Enabling plugin: buildingBlocks");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Safely disabling plugin: buildingBlocks");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return true;
+	}
 
 }
